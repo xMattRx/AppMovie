c14e9eaa2f171eab04891de5bd8573c44421c5ef
Matthew Nascimento <matthewromie@hotmail.com>
2021-06-25 14:30:58 -0300

    Pagina inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react'
+import Movie from './components/Movie/Movie';
+
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=df7ac8100f0dbc1e726f955647170d0a&page=1";
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=df7ac8100f0dbc1e726f955647170d0a&query="
+
+
 
 function App() {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setMovies(data.results);
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <input className="search" type="search" placeholder="Search..." />
       </header>
-    </div>
+      <div className="movie-container" >
+
+        {movies.length > 0 && movies.map((movie) => {
+          return <Movie key={movie.id} {...movie} />
+        })}
+      </div>
+    </>
   );
 }
 
